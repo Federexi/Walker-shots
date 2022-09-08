df66fdcd4939ee56fbdb5c0c74ecdc1f6566b655
federico bidarra <federicobidarra@gmail.com>
2022-09-07 23:11:04 -0300

    Creo Navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import twitter from './twitter.png';
+import instagram from './instagram.png';
+import man from './striding-man.jpg';
+
+const Blanco = { color: 'white' };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <nav className="navbar navbar-expand-lg navbar-light nav-fondo">
+   <div className="container-fluid">
+     <a style={Blanco} className="navbar-brand" href="#">
+      Walker Shots
+      <img src={man} alt="Hombre caminando" width="30" height="24" class="d-inline-block align-text-top"></img>
+      </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div className="navbar-nav mx-auto">
+        <a style={Blanco} className="nav-link active" aria-current="page" href="#">Vinos</a>
+        <a style={Blanco} className="nav-link active" href="#">Licores</a>
+        <a style={Blanco} className="nav-link active" href="#">Cervezas</a>
+      </div>
+      <ul className="nav navbar-nav flex-row justify-content-md-center justify-content-start flex-nowrap">
+            <li className="nav-item"><a className="nav-link" href=""> <img src={twitter} alt="twitter"></img> </a> </li>
+            <li className="nav-item"><a className="nav-link" href=""> <img src={instagram} alt="instagram"></img> </a> </li>
+      </ul>
     </div>
+   </div>
+  </nav>
   );
 }
 
