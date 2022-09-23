2cfc719e112892e1077969a0ce860f79a1714534
federico bidarra <federicobidarra@gmail.com>
2022-09-23 17:42:49 -0300

    Bug fixes

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -8,7 +8,6 @@ function ItemCount({stock, initial, onAdd}) {
             return
         }
         setNumber(number+1)
-        onAdd(number+1)
     }
 
     const handleNumberMin = () => {
@@ -16,19 +15,15 @@ function ItemCount({stock, initial, onAdd}) {
             return
         }
         setNumber(number-1)
-        onAdd(number-1)
     }
 
-    const addToCart = () => {
-        alert(`Has añadido ${number} productos`)
-    }
 
     return (
         <div className="d-grid gap-2 d-md-block">
             <button className="btn btn-primary resta" type="button" onClick={handleNumberMin}>-</button>
             {number}
             <button className="btn btn-primary suma" type="button" onClick={handleNumberSum}>+</button>
-            <button className="btn btn-primary agregar" type="button" onClick={addToCart}>Add to cart</button>
+            <button className="btn btn-primary agregar" type="button" onClick={() => onAdd(number)}>Add to cart</button>
         </div>
  )  
 }
